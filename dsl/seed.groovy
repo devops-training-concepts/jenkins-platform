21b69e7bd8f1be0e5760aa0ed247e87d0c88be0e
balajivb25 <balajiv.b25@gmail.com>
2026-04-26 13:20:53 +0900

    changes made for jenkins setup

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,19 +1,21 @@
 folder('apps')
 
-['app1-repo','app2-repo'].each { repo ->
-  pipelineJob("apps/${repo}") {
-    definition {
-      cpsScm {
-        scm {
-          git {
-            remote {
-              url("https://github.com/devops-training-concepts/${repo}.git")
+['app1-repo', 'app2-repo'].each { repo ->
+
+    pipelineJob("apps/${repo}") {
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url("https://github.com/devops-training-concepts/${repo}.git")
+                        }
+                        branch('main')
+                    }
+                }
+                scriptPath('Jenkinsfile')
             }
-            branch('main')
-          }
         }
-        scriptPath('Jenkinsfile')
-      }
     }
-  }
 }
